2715e7657167ad6c7e88fff42b7dc8c8544ba821
Suppandi Kappis <suppandikappis@Suppandis-MacBook-Air.local>
2018-11-02 16:12:21 +0000

    taking jacket potato only order or sw order but not both (issue with req)

--- snack-shack/order.py
+++ snack-shack/order.py
@@ -4,10 +4,12 @@ import json
 
 SCHEDULE = []
 ITEM_TASK_INFO = {'s': [("make", 60), ("serve", 30)],
-                  'j': [("make", 60), ("serve", 30)],
+                  'j': [("heat", 150), ("top", 30), ("serve", 30)],
                   }
 
-MAX_WAIT_TIME = 5 * 60
+MAX_WAIT_TIME = {'s': 5 * 60,
+                 'j': 7*60
+                 }
 INVENTORY_FILE = "inventory.json"
 INVENTORY = {}
 
@@ -25,10 +27,9 @@ def update_inventory():
 
 
 def display_inventory():
-    print("Inventory Check: We can make max no of the following items")
     for i, n in INVENTORY.items():
-        print("{} {}".format(n, item_name(i)))
-
+        if n >0:
+            print("{} {}".format(n, item_name(i)))
 
 def display_schedule():
     for info in SCHEDULE:
@@ -44,9 +45,13 @@ def item_name(code):
 def order(no, item='s'):
     item_tasks = ITEM_TASK_INFO[item]
     retrieve_inventory()
+    max_wait = MAX_WAIT_TIME[item]
     for order_no in range(1, no + 1):
         if INVENTORY[item] < 1:
             print("Sorry! {} is SOLD OUT".format(item_name(item)))
+            if sum(INVENTORY.values()) > 0:
+                print("Alternatives:")
+                display_inventory()
             break
         start = 0
         if SCHEDULE:
@@ -54,7 +59,7 @@ def order(no, item='s'):
         tasks = []
         for task, time in item_tasks:
             finish = start + time
-            if finish > MAX_WAIT_TIME:
+            if finish > max_wait:
                 print("Sorry! too busy can't take more orders")
                 update_inventory()
                 return
@@ -73,7 +78,12 @@ def order(no, item='s'):
 
 
 
-order(4)
+order(3,'j')
 display_schedule()
-display_inventory()
+
+print("Inventory Check:")
+if sum(INVENTORY.values()) > 0:
+    display_inventory()
+else:
+    print("All SOLD-OUT")
 
